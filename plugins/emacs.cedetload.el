d7f497d8a957b00e2f8a85756ad411d9456a7f95
Ashkan Ghassemi <ghassemi@ftml.net>
2014-12-07 04:47:49 +0330

    fixed a typo in plugins/emacs.cedetloads.el and install script for linux.

--- plugins/emacs.cedetload.el
+++ plugins/emacs.cedetload.el
@@ -1,4 +1,4 @@
 ;; CEDET Load
-(load-file "/Users/ashkan/tools/cedet/cedet-remove-builtin.el")
-(load-file "~/tools/cedet/cedet-devel-load.el")
-(load-file "~/tools/cedet/contrib/cedet-contrib-load.el")
+(load-file "~/Tools/cedet/cedet-remove-builtin.el")
+(load-file "~/Tools/cedet/cedet-devel-load.el")
+(load-file "~/Tools/cedet/contrib/cedet-contrib-load.el")
